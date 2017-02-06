cc0ebe847ed0911171d0611e26e24fcadbda20f6
matmax <matthias.graf@st.ovgu.de>
2017-02-06 11:56:35 +0100

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Structured Sutras</title>
+	<link rel="stylesheet" type="text/css" href="main.css"/>
+	<link rel="icon" type="image/png" href="assets/Enso.svg.png">
+	<!-- deferred scripts are guarenteed to execute in the order that they appear in the document -->
+	<script src="main.js" defer></script>
+</head>
+
+<body>
+	<!--<header>Structured Sutras</header>-->
+	
+	<?php include 'transcript.xml'; ?>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+	Matthias Graf
+	matthias.graf@mgrf.de
+	2016
+	GNU AGPL v3
+*/
+
+console.log("hello world")
