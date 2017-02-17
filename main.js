93ef05c834ad31402486989eaac6f582645666a6
matmax <matthias.graf@st.ovgu.de>
2017-02-17 16:43:38 +0100

    preparing release

--- main.js
+++ main.js
@@ -5,4 +5,21 @@
 	GNU AGPL v3
 */
 
-console.log("hello world")
+var showMoreOn = false
+var aboutSummary = document.getElementById("aboutSummary")
+var showMore = document.getElementById("showMore")
+var aboutFull = document.getElementById("aboutFull")
+
+console.assert(aboutSummary && showMore && aboutFull)
+
+aboutSummary.onclick = function() {
+	aboutFull.style.maxHeight = showMoreOn ? "0px" : "3000px"
+	if (showMoreOn) {
+		showMore.classList.remove("on")
+	} else {
+		showMore.classList.add("on")
+	}
+	showMoreOn = !showMoreOn
+}
+
+console.log("done.")
